29e6802250632ef00ff56fa8238815c8302110dd
Dexterte <rejimully@gmail.com>
2023-10-22 19:30:13 +0900

    ref:マジックナンバーを定数に置き換え

--- mod26.py
+++ mod26.py
@@ -1,18 +1,18 @@
 text = input()
 SHIFT_AMOUNT = 13
+ALPHABET_SIZE = 26
 
 
 def rot13(target: str) -> str:
     """
     Rot13暗号
     13文字づつずらす
-
     """
     if 'a' <= target <= 'z':
-        shifted_char = (ord(target) - ord('a') + SHIFT_AMOUNT) % 26
+        shifted_char = (ord(target) - ord('a') + SHIFT_AMOUNT) % ALPHABET_SIZE
         return chr(shifted_char + ord('a'))
     elif 'A' <= target <= 'Z':
-        shifted_char = (ord(target) - ord('A') + SHIFT_AMOUNT) % 26
+        shifted_char = (ord(target) - ord('A') + SHIFT_AMOUNT) % ALPHABET_SIZE
         return chr(shifted_char + ord('A'))
     return target
 
